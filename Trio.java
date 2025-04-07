9f02089392057655cdaec5e65298795e50eec2c0
cHurm16 <chloe.hurm@stu.daviess.kyschools.us>
2025-04-07 14:27:16 +0000

    homework

--- Trio.java
+++ Trio.java
@@ -1,1 +1,45 @@
 //Write your answer here
+
+public class Trio implements MenuItem
+{
+    private Sandwhich sandwhich; 
+    private Salad salad; 
+    private Drink drink; 
+
+    public Trio(Sandwhich pbj, Salad ceaser, Drink drpepper)
+    {
+        sandwhich = pbj;
+        salad = ceaser; 
+        drink = drpepper; 
+    }
+
+    public String getName()
+    {
+        return sandwhich.getName() + "/" + salad.getName() + "/" + drink.getName() + "Trio";
+
+    }
+
+    public double getPrice()
+    {
+        MenuItem cheapest= sandwhich;
+
+        if(salad.getPrice()<cheapest.getPrice())
+        cheapest = salad; 
+        if(drink.getPrice()<cheapest.getPrice())
+        cheapest = drink; 
+
+        return sandwhich.getPrice()+salad.getPrice()+drink.getPrice()-cheapest.getPrice();
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
